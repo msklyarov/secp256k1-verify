b48d81b835756d684afe880bf36e20104e3244f5
Maxim Sklyarov <sklyarov.ma@gmail.com>
2023-06-15 09:08:25 +0300

    Fix address calculation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const elliptic = require("elliptic");
 const crypto = require("crypto");
+const sha3 = require("js-sha3");
 
 const createHash = crypto.createHash;
 const ec = new elliptic.ec("secp256k1");
@@ -12,7 +13,7 @@ app.post("/verify", function (req, res) {
   const { address, signedAddress, publicKey } = req.body;
 
   try {
-    const addressFromPubKey = createHash("rmd160").update(publicKey).digest().toString("hex");
+    const addressFromPubKey = createHash("rmd160").update(sha3.sha3_256(publicKey)).digest().toString("hex");
 
     res.json({
       success:
@@ -28,4 +29,4 @@ app.use("*", function (req, res) {
   res.send("Wrong route");
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
